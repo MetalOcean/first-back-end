fe760d5088f170944daf299b1f1b8e0b014aed82
MetalOcean <lucaaas907@gmail.com>
2024-04-08 14:28:09 -0300

    chore: asdgiuasuih

--- Teste.js
+++ Teste.js
@@ -1,1 +1,12 @@
-console.log('teste')
+const express = require('express')
+const app = express()
+const Port = 3000
+
+app.get(`/`, (req, res) => {
+    console.log(`ENTROU AQUI`)
+ res.send(`SALVE RAPAZIADA`)   
+})
+
+app.listen(Port, () => {
+    console.log(`Meu texto ${Port}`) 
+})
